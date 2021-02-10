f7ff42890cf9392a9f8ad27e206c1cf88502d6cb
Nick Aristidou <nick.aristidou@convexin.com>
2021-02-10 17:37:27 +0000

    initializing repository

--- Lecture5.R
+++ Lecture5.R
@@ -6,3 +6,8 @@ hist(chickwts$weight)
 boxplot(chickwts$weight ~ chickwts$feed)
 
 lattice::histogram(~weight, data=chickwts)
+
+lattice::histogram(~mpg, data=mtcars)
+lattice::histogram(~mpg | factor(cyl), data=mtcars)
+#if factor is a continuous variable:
+lattice::histogram(~mpg | wt, data=mtcars)
