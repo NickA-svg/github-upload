abc3c5e4644760cbe3439054a19d934cd910b5b6
Nick Aristidou <nick.aristidou@convexin.com>
2021-02-10 13:35:36 +0000

    Commit completed Data types

---
+++ Udemy_Data_vis.R
@@ -0,0 +1,23 @@
+#Data Types ----------------------------------------------------------------------------------------
+#logical - true or false values
+4 == 4 
+
+#integer - whole numbers 
+a <- 4
+class(a)
+
+b <- 4L
+class(b)
+
+#Numeric all numbers are included
+c <- 3.45
+class(c)
+
+#complex numbers also exist - ref stats
+f <- complex(real=1,imaginary = 1)
+class(f)
+f
+
+#Characters 
+e <- "Nick Aristidou"
+class(e)
